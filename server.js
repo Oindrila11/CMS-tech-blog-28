0a91284dbaeb10296278c5b2eccf926783a88b6f
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-06-07 18:20:05 -0500

    added homepage route from homework module

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
-
+const exphbs = require('express-handlebars');
 const app = express();
+
 const PORT = process.env.PORT || 3006;
+const hbs = exphbs.create({});
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
@@ -15,4 +25,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
