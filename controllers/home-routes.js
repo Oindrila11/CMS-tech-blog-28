12f6b1b503cdb5acae88d312214aa74323364922
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-06-08 10:16:38 -0500

    added comments from homework module

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,13 +30,15 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
-    });
+    }); 
 });
 
 router.get('/login', (req, res) => {
